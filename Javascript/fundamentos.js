1ea7d063fc736716eb7263538843cb159176e1b7
Hectorvegaloza <vega314212@gmail.com>
2024-03-07 19:49:04 -0500

    Practica

---
+++ Javascript/fundamentos.js
@@ -0,0 +1,34 @@
+//alert("works!")
+/*console.log("Este es un mensaje para la consola")
+document.write("Mensaje en interfaz de usuario")
+*/
+
+//VARIABLES
+
+// no recomendadas
+/* nombre = "asinacion"
+console.log(nombre)
+var salud2 = "hola"
+console.log(salud2)
+ */
+//Recomendas en tipos de datos String
+/* let Saludo3 = "final";
+console.log(Saludo3);
+const saludo4 = "saludo real"; */
+ 
+//variables globales
+/* 
+let global = "arroz con habichuela";
+console.log(global);
+
+{ // Variable local
+    const gvvl = "arroz con pollo";
+    console.log(gvvl);
+}
+
+let numero1 = 1;
+const numero2 = 2; // el valor contenido con esa variable no se puede reasignar un valor
+console.log(numero1);
+console.log(numero2); */
+
+// "use strict"
