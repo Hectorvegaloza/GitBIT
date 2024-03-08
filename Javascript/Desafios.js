ead64e3f26e8414b64360040c41f919316494625
Hectorvegaloza <vega314212@gmail.com>
2024-03-07 22:04:59 -0500

    Javascript

---
+++ Javascript/Desafios.js
@@ -0,0 +1,12 @@
+"use strict"
+// document.write("Hola");
+//onsole.log("Hola");c
+
+
+const entrada = prompt("Ingrese su nombre:");
+
+if(entrada){
+    document.write("Hola "+ entrada + "!");
+}else{
+    document.write("Hola");
+}
--- Javascript/fundamentos.js
+++ Javascript/fundamentos.js
@@ -1,34 +1,121 @@
-//alert("works!")
-/*console.log("Este es un mensaje para la consola")
-document.write("Mensaje en interfaz de usuario")
+// Este es un comentario de JavaScript de una sola línea
+/*
+Este es un
+comentario de
+varia líneas
 */
 
-//VARIABLES
+/* Métodos de interacción */
+// Entrada
+/* let entrada = prompt();
+entrada = prompt("Instrucciones");
+console.log(entrada); */
+// Salida
+/* console.log("mensaje en consola")
+document.write("mensaje en interfaz de usuario")
+alert("mensaje en ventana emergente") */
 
-// no recomendadas
-/* nombre = "asinacion"
-console.log(nombre)
-var salud2 = "hola"
-console.log(salud2)
- */
-//Recomendas en tipos de datos String
-/* let Saludo3 = "final";
-console.log(Saludo3);
-const saludo4 = "saludo real"; */
- 
-//variables globales
-/* 
-let global = "arroz con habichuela";
-console.log(global);
+/* VARIABLES */
+// Declaración
+/* saludo1 = "hola1"
+var saludo2 = "hola2"
+let saludo3 = "hola3"
+const saludo4 = "hola4"
+console.log(saludo1)
+console.log(saludo2)
+console.log(saludo3)
+console.log(saludo4) */
 
-{ // Variable local
-    const gvvl = "arroz con pollo";
-    console.log(gvvl);
+// Ámbito de las variables
+/* let variableGlobal = "soy variable global"
+console.log(variableGlobal)
+{
+  let variableLocal = "soy variable local"
+  console.log(variableLocal)
+} */
+
+/* let cambiaValor = 1;
+const noCambiaValor = 2;
+cambiaValor = "1";
+noCambiaValor = 2;
+console.log(cambiaValor)
+console.log(noCambiaValor) */
+
+/* Modo estricto */
+"use strict"
+/* saludo = "hola";
+console.log(saludo); */
+
+/* Tipos de dato primitivos */
+// string
+let miVariable = "";
+ miVariable = "este es un texto de 'ejemplo 1'";
+ miVariable = 'este es un texto de "ejemplo 2"';
+ miVariable = `este es un 'texto' de "ejemplo" 3`;
+ // number
+ miVariable = 10;
+ miVariable = 9.5;
+ miVariable = 0xFF;
+  miVariable = 123e5;
+// boolean
+ miVariable = true;
+ miVariable = false;
+ // undefined
+ let miVariableSinAsignacion;
+ //console.log(miVariableSinAsignacion);
+ // null
+ miVariable = null;
+// console.log(miVariable);
+
+/* Expresiones booleanas */
+// Truthy o verdaderos
+/* console.log(true)
+console.log(1)
+console.log(-1)
+console.log("a") */
+// Falsy o falsos
+/* console.log(false)
+console.log(0)
+console.log("")
+console.log(undefined)
+console.log(null) */
+
+/* Control de flujo */
+// Condicionales
+/* if (true) {
+  console.log("se cumple la condición 1")
 }
 
-let numero1 = 1;
-const numero2 = 2; // el valor contenido con esa variable no se puede reasignar un valor
-console.log(numero1);
-console.log(numero2); */
+if (false) {
+  console.log("se cumple la condición 2")
+} else {
+console.log("no se cumple la condición 2")
+}
 
-// "use strict"
+if (false) {
+  console.log("se cumple la condición 3 a")
+} else if (true) {
+  console.log("se cumple la condición 3 b")
+} else {
+console.log("no se cumple la condición 3")
+}
+
+if (false) {
+  console.log("se cumple la condición 4")
+}
+if (true) {
+  console.log("se cumple la condición 5")
+} */
+
+/* const perfil = "profesor"
+switch (perfil){
+  case "estudiante":
+    console.log("eres un estudiante")
+    break;
+  case "profesor":
+    console.log("eres un profesor")
+    break;
+  default:
+    console.log("perfil no permitido")
+    break;
+} */
