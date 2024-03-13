69ea72a63af31a57f7ed92a6a2e1d3ec5cc37128
Hectorvegaloza <vega314212@gmail.com>
2024-03-12 21:35:26 -0500

    DESAFIOS CULMINADOS

--- Javascript/Desafios.js
+++ Javascript/Desafios.js
@@ -2,27 +2,37 @@
 
 // Primer desafio Saludar
 // document.write("Hola");
-//Console.log("Hola");c
+// Console.log("Hola");c
 
 /* 
 const entrada = prompt("Ingrese su nombre:");
 
+if (entrada >= 0 || entrada <-1  ){
+    document.write("Paisano, la entrada es un numero deber ser su nombre");
+}else{
 if(entrada){
     document.write("Hola "+ entrada + "!");
 }else{
     document.write("Hola");
+}
 } */
-/* 
-const entrada = prompt("Ingrese su nombre:");
 
+
+/* const entrada = prompt("Ingrese su nombre:");
+
+if (entrada >= 0 || entrada <-1  ){
+  document.write("Paisano, la entrada es un numero deber ser su nombre");
+}else{
 switch (entrada){
   case "":
-    console.log("Hola"); 
+    document.write("Hola"); 
     break;
   default:
-    console.log("Hola "+ entrada + "!________");
+    document.write("Hola "+ entrada + "!");
     break;
-}  */
+}  
+}*/
+
 
 // Hacer un programa que pida dos numeros al usuario y los sume
 
@@ -30,27 +40,46 @@ switch (entrada){
 
 num1 = Number(prompt("Digite el numero 1"));
 num2 = Number(prompt("Digite el numero 2"));
-console.log(num1 + num2);
-//template string
-document.write(`la suma de ${num1} y ${num2} es ${num1+num2}`);
 
+if(isNaN(num1) || isNaN(num2)){
+  document.write("Paisano, el valor ingresado no es un número");
+}else{
+  console.log(num1 + num2);
+  //template string
+  document.write(`la suma de ${num1} y ${num2} es ${num1+num2}`);
+}
  */
+
+
 
 // Hacer un programa que pida aparte nombre y apellido aparte y muestre el nombre completo
 
 /* 
+let num1, num2,resultado;
+
 num1 = prompt("Digite Nombre");
-num2 = prompt("Digite apellido");
-resultado = num1 + " " + num2; 
-console.log(resultado);
+num2 = prompt("Dighectorite apellido");
+
+if (isNaN(num1) && isNaN(num2)){
+  resultado = num1 + " " + num2; 
+  document.write(`Su nombre completo es: ${resultado}`);
+}else{
+  document.write("Paisano, la entrada es un numero, deber ser su nombre");
+}
  */
+
+
 
 // hacer un programa que reciba un numero y muestre si es cero si es positivo o si es negativo
 
-/* let num1;
+/*  let num1;
 
 num1 = Number(prompt("Digite un número:"));
 
+
+if(isNaN(num1)){
+  document.write("Paisano, el valor ingresado no es un número");
+}else{
 if(num1 === 0 ) {
   document.write(`Usted ingresó el numero Cero, ${num1}`);
 } else if(num1>1) {
@@ -58,7 +87,7 @@ if(num1 === 0 ) {
 } else if (num1<0) {
   document.write(`Usted ingresó un numero negativo, ${num1}`);
 }
-
+}
  */
 
 //hacer un programa que reciba del usuario 2 numeros y muestre cual es el nunero mayor y cual es menor
@@ -67,9 +96,11 @@ let num1, num2;
 
 num1 = Number(prompt("Digite el numero 1"));
 num2 = Number(prompt("Digite el numero 2"));
-
-if(num1 === 0 || num2 ===0 || num1==num2) {
-  document.write(`ERROR!!! Algunos de los numeros es Cero o son iguales compa`);
+if(isNaN(num1)&&isNaN(num2)){
+  document.write("Paisano, el valor ingresado no es un número");
+}else{
+if(num1 === 0 || num2 ===0 || num1==num2 || num2 <0|| num1 <0) {
+  document.write(`ERROR!!! Algunos de los numeros es Cero o son iguales compa o son negativos`);
 }else{
 if(num1 > num2) {
   document.write(`El mayor es ${num1} y el menor es ${num2}`);
@@ -77,7 +108,8 @@ if(num1 > num2) {
   document.write(`El mayor es ${num2}  y el menor es ${num1}`);
 }
 }
-
-
+}
+ 
+ 
 
 
